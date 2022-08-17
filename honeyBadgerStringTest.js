54b6f5b940f1da8bb9b90c52ffbaf38516fc2c4b
Ben Whittaker <bluetoaster42@gmail.com>
2022-08-16 18:55:14 -0600

    Add the day of the week to the hello world alert

---
+++ honeyBadgerStringTest.js
@@ -0,0 +1,2 @@
+let theString = "hoNeY BaDGer";
+console.log(`It is ${theString.toLowerCase().includes("badger")} that the string "hoNeY BaDGeR" contains the string "badger" without regard to case.`);
