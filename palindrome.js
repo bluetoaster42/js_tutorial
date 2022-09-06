13b4bfece54790ab0c9b4f48040c828e6c8ba736
Ben Whittaker <bluetoaster42@gmail.com>
2022-09-06 15:07:05 -0600

    Exercise 7.1.1: add the louder() function to Phrase

--- palindrome.js
+++ palindrome.js
@@ -5,12 +5,19 @@ function reverse(string){
 
 
 
-// Defines a Phrase object
+// Defines a Phrase object, from section 7.1
 function Phrase(content){
 	this.content = content;
 	
 	this.palindrome = function palindrome(){
 		let processedContent = content.toLowerCase();
 		return processedContent === reverse(processedContent);
+	}
+	
+	//Exercise 7.1.1
+	//makes the phrase LOUDER
+	this.louder = function(){
+		let louderContent = content.toUpperCase();
+		return louderContent;
 	}
 }
