de973515ab64c7001e48a7eca77a19703401ae34
Ben Whittaker <baw439@usask.ca>
2022-09-18 17:48:06 -0600

    Listing 9.9 - event listener for loading of the DOM

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 let Phrase = require("mhartl-palindrome");
 
+function plaindromeTester() {
+    let string = prompt("Please enter a string for palindrome testing");
+    let phrase = new Phrase(string);
 
-let string = prompt("Please enter a string for palindrome testing");
-let phrase = new Phrase(string);
-
-if (phrase.palindrome()){
-    alert(`"${phrase.content}" is a palindrome!`);
-}else{
-    alert(`"${phrase.content}" is NOT a palindrome...`);
+    if (phrase.palindrome()) {
+        alert(`"${phrase.content}" is a palindrome!`);
+    } else {
+        alert(`"${phrase.content}" is NOT a palindrome...`);
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    let button = document.querySelector("#palindromeTester");
+    button.addEventListener("click", plaindromeTester);
+});
